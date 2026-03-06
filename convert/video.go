0f8df2efc722a2ad61848f410b432980499c438e
almeidafm <132709094+almeidafm@users.noreply.github.com>
2026-03-06 20:01:26 -0300

    video

--- convert/video.go
+++ convert/video.go
@@ -1,8 +1,27 @@
 package convert
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"path/filepath"
+)
 
 func Video(inputFiles []string, toFormat string) error {
-	fmt.Println("video")
+	for _, input := range inputFiles {
+
+		ext := filepath.Ext(input)
+		name := input[:len(input)-len(ext)]
+		output := name + "." + toFormat
+
+		cmd := exec.Command("ffmpeg", "-i", input, output)
+
+		err := cmd.Run()
+		if err != nil {
+			return fmt.Errorf("conversion failed for %s: %w", input, err)
+		}
+
+		fmt.Println("converted:", output)
+	}
+
 	return nil
 }
